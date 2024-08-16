3a9c5e6b648e08d9ed30c388f8db08ff496e94da
Michael Bock <no-day@posteo.net>
2024-08-16 18:02:04 +0200

    Remove lima

--- tests/test.hs
+++ tests/test.hs
@@ -6,7 +6,7 @@ import Data.String.Conversions (cs)
 import Data.Text (replace)
 import GHC.Generics
 import qualified GHC.Generics as GHC
-import Lima.Converter (Format (..), convertTo, def)
+--import Lima.Converter (Format (..), convertTo, def)
 import qualified Readme
 import Relude
 import Spec (unitTests)
@@ -16,33 +16,32 @@ import Text.Regex (mkRegex, subRegex)
 
 main :: IO ()
 main = do
-  genReadme
   defaultMain tests
 
-genReadme :: IO ()
-genReadme = do
-  readmeMd <- readFileBS "README.md"
+-- genReadme :: IO ()
+-- genReadme = do
+--   readmeMd <- readFileBS "README.md"
 
-  readmeHs <- readFileBS "tests/Readme.hs"
+--   readmeHs <- readFileBS "tests/Readme.hs"
 
-  let readmeExample = convertTo Hs Md def (cs readmeHs)
+--   let readmeExample = convertTo Hs Md def (cs readmeHs)
 
-  Readme.main
+--   Readme.main
 
-  readmeOutputRust <- readFileBS "tests/outputs/demo.rs"
-  readmeOutputTypeScript <- readFileBS "tests/outputs/demo.ts"
+--   readmeOutputRust <- readFileBS "tests/outputs/demo.rs"
+--   readmeOutputTypeScript <- readFileBS "tests/outputs/demo.ts"
 
-  let readmeMd' =
-        cs readmeMd
-          & replaceSection "example" readmeExample
-          & replaceSection
-            "exampleOutRust"
-            ("```rust\n" <> cs readmeOutputRust <> "\n```")
-          & replaceSection
-            "exampleOutTypeScript"
-            ("```ts\n" <> cs readmeOutputTypeScript <> "\n```")
+--   let readmeMd' =
+--         cs readmeMd
+--           & replaceSection "example" readmeExample
+--           & replaceSection
+--             "exampleOutRust"
+--             ("```rust\n" <> cs readmeOutputRust <> "\n```")
+--           & replaceSection
+--             "exampleOutTypeScript"
+--             ("```ts\n" <> cs readmeOutputTypeScript <> "\n```")
 
-  writeFileBS "README.md" (cs readmeMd')
+--   writeFileBS "README.md" (cs readmeMd')
 
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
